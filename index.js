c6e0a3dab45ff1646da7b9411ffd8bcdae68fc14
ImaDuhf <timperson234@gmail.com>
2022-12-09 16:07:39 +0100

    Added eventlisteners

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 /* Listeners and setting player names */
+let symbols = ['X', 'O'];
+let playerTurn = 0;
+let turns = 0;
+let playerOne = document.querySelector('.player-one');
+let playerTwo = document.querySelector('.player-two'); //.value för att få värdet sen
+let startButton = document.querySelector('.start-btn');
+startButton.addEventListener('click', () => {
+    console.log(
+        `Player one: ${playerOne.value}, Player two: ${playerTwo.value}`
+    );
+});
+
+let gameBoard = document.querySelector('.gameboard');
+
+gameBoard.addEventListener('click', (event) => {
+    if (event.target.classList != 'gameboard') {
+        event.target.innerText = symbols[playerTurn];
+        playerTurn = (playerTurn + 1) % 2;
+        turns++;
+    }
+    if (turns == 9) {
+        setTimeout(() => {
+            alert('Game Over!');
+        }, 300);
+    }
+});
