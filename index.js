86050062f24708cf483b2cb4796935596479411d
Jakob Rolandsson <jakobrolandsson@Jakobs-Air.local>
2022-12-14 10:49:43 +0100

    added game update on click and expecting list modifications to work by next release

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ let playerTwo = document.querySelector('.player-two'); //.value för att få vä
 
 let gameBoard = document.querySelector('.gameboard');
 
+function getBoardList() {
+  let board = []
+  //Hämta symbolerna på varje kort och lägg dem i en lista
+  return board;
+}
+
+function renderFromList(board) {
+  // Uppdatera korten på spelplanen med symbolerna i board
+}
+
 gameBoard.addEventListener('click', (event) => {
   if (event.target.classList != 'gameboard') {
     event.target.innerText = symbols[playerTurn];
@@ -24,18 +34,37 @@ gameBoard.addEventListener('click', (event) => {
       alert('Game Over!');
     }, 300);
   }
+
+  // let board = ["", "X", "", "X", "", "", "O", "", ""];
+  let board = getBoardList();
+  game.update(idValue, board);
 });
 
 
 const createRoomBtn = document.querySelector(".create-room-btn");
+const roomIdTxt = document.querySelector(".room-id-txt");
+
+const joinRoomBtn = document.querySelector(".join-room-btn");
+const roomIdField = document.querySelector(".room-id-field");
+
+let idValue = -1;
+
 
 function updateHandler(data) {
-  console.log(data);
-}
+  roomIdTxt.textContent = "Room id: " + data.id;
+  idValue = data.id;
 
+  renderFromList(data.board);
+}
 
 function createRoom() {
   game.create(updateHandler)
 }
 
 createRoomBtn.addEventListener('click', createRoom);
+
+function joinRoom() {
+  game.join(roomIdField.value, updateHandler);
+}
+
+joinRoomBtn.addEventListener('click', joinRoom);
